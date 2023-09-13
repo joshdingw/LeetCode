1ffb23af83ace1385595155ca499bd8b5a227863
joshdingw <142236003+joshdingw@users.noreply.github.com>
2023-09-14 07:49:39 +0800

    Time: 1 ms (85.79%), Space: 39.3 MB (71.65%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -3,40 +3,27 @@ import java.util.HashSet;
 
 class Solution {
     public boolean isHappy(int n) {
-        int length = String.valueOf(n).length();
-        int[] nums = nToArray(n);
         
         Set<Integer> record = new HashSet<>();
-        int sum = squareSum(nums);
-        record.add(sum);
+
+        int sum = changeN(n);
+
         while(sum != 1){
-            nums = nToArray(sum);
-            sum = squareSum(nums);
             if(record.contains(sum)){
                 return false;
-            }else{
-                record.add(sum);
             }
+            record.add(sum);
+            sum = changeN(sum);
         }
-
         return true;
+    }
 
-        
-    }
-    public int[] nToArray(int n){
-        int length = String.valueOf(n).length();
-        char[] numsChar= (Integer.toString(n)).toCharArray();
-        int[] nums =  new int[length];
-        for(int i = 0 ; i < length; i++){
-            nums[i] = Character.getNumericValue(numsChar[i]);
+    public int changeN(int n){
+        int result = 0;
+        while(n != 0){
+            result += (n % 10) * (n % 10);
+            n = n / 10;
         }
-        return nums;
-    }
-    public int squareSum(int[] nums){
-        int sum = 0;
-        for(int i = 0; i < nums.length; i++){
-            sum = sum + nums[i] * nums[i];
-        }
-        return sum;
+        return result;
     }
 }
