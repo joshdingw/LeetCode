8b04995c1fe7836b4832edf5aa47bc5ddd7cff68
joshdingw <142236003+joshdingw@users.noreply.github.com>
2023-09-25 03:43:10 +0800

    Time: 0 ms (100%), Space: 45.8 MB (33.32%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
@@ -21,28 +21,7 @@ class Solution {
 
         TreeNode left = root.left;
         TreeNode right = root.right;
-        if(left != null && right != null){
-            return helper(left) + helper(right) + 3;
-        }else if(left != null || right != null){
-            return helper(left) + helper(right) + 2;
-        }
-        
-        return 1;
+        return 1 + countNodes(left) + countNodes(right);
     }
-    public int helper(TreeNode cur){
-         if(cur == null){
-            return 0;
-        }
-
-        TreeNode left = cur.left;
-        TreeNode right = cur.right;
-        if(left != null && right != null){
-            return helper(cur.left) + helper(cur.right) + 2;
-        }else if(left != null || right != null){
-            return helper(cur.left) + helper(cur.right) + 1;
-        }
-        
-        return helper(cur.left) + helper(cur.right);
-
-    }
+ 
 }
